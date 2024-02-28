b5aa2b697114a205834218fa358a3d568cc993b7
Thiago Goetten <Thiago Goetten@ThyeKah>
2024-02-28 15:08:20 -0300

    commit atualizacao 0.1

---
+++ src/main/java/thiago/loja/virtual/model/MarcaProduto.java
@@ -0,0 +1,42 @@
+package thiago.loja.virtual.model;
+
+import java.io.Serializable;
+
+import org.hibernate.annotations.GeneratorType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	private Long id;
+
+	private String NomeDesc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return NomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		NomeDesc = nomeDesc;
+	}
+
+}
