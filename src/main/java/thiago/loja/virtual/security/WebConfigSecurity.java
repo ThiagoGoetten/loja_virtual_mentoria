a7964c89503b82280b2ccee1176e7314a5e58502
Thiago Goetten <Thiago Goetten@ThyeKah>
2024-03-25 09:12:34 -0300

    commit 1.2 Erro Teste Security

--- src/main/java/thiago/loja/virtual/security/WebConfigSecurity.java
+++ src/main/java/thiago/loja/virtual/security/WebConfigSecurity.java
@@ -1,13 +1,11 @@
 package thiago.loja.virtual.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import jakarta.servlet.http.HttpSessionListener;
 
@@ -15,11 +13,12 @@ import jakarta.servlet.http.HttpSessionListener;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener{
-	
+	@Override
 	public void configure(WebSecurity web) throws Exception {
+		
 		web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso")
 		.antMatchers(HttpMethod.POST, "/salvarAcesso");
-				
+		
 	}
-	
+
 }
